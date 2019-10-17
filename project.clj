a0851aa01cb861ed265cce43955b6b945d4164a4
Mikael Reponen <mikael.reponen@gmail.com>
2019-10-17 20:30:29 +0300

    initial commit

---
+++ project.clj
@@ -0,0 +1,11 @@
+(defproject b17 "0.1.0-SNAPSHOT"
+    :description "B17"
+    :url "https://github.com/Viritystila/b17"
+    :license {:name "The Unlicense"
+            :url "http://unlicense.org"}
+    :dependencies [[org.clojure/clojure "1.9.0"]
+                    [trigger "0.1.0-SNAPSHOT"]
+                    [org.viritystila/viritystone "0.0.1-SNAPSHOT"]
+                    [rm-hull/markov-chains "0.1.1"]]
+                    
+     :jvm-opts ^:replace [])
